207240fad9e3cfbe609c829ee89cd83c00ec9a43
Ari Markowitz <ariymarkowitz@gmail.com>
2022-08-05 12:54:41 +1200

    Update examples

--- tree-examples.m
+++ tree-examples.m
@@ -8,14 +8,22 @@ print Origin(tree);
 
 v := BTTVertexFromMatrix(tree, M ! [1, 2, 3, 22]);
 print v;
-print DistanceToOrigin(v);
+print Distance(v, Origin(tree));
+print Matrix(v);
 
 A := M ! [2, 0, 1, 3];
-print A*v;
+print v*A;
 print TranslationLength(A);
+print Distance(v, v*A^2);
+print Path(v, v*A^2);
+print TranslationAxisBoundary(A);
+w := ProjectionOntoMinTranslationSet(tree, A, Vector(Qp, [1, 9]));
+print w;
+print IsInMinTranslationSet(A, w);
 
 B := M ! [5, 3, 1, 3];
 print TranslationLength(B);
+print IsInMinTranslationSetBoundary(B, Vector(Qp, [1, 1]));
 print FixSet(tree, B);
 
 print FixSet(tree, M ! [1, 0, 0, 1]);
@@ -24,3 +32,12 @@ print FixSet(tree, M ! [0, 1, -1, -2]);
 print FixSet(tree, M ! [2, 1, 1/8, 5]);
 print FixSet(tree, M ! [2, 1, 1/16, 5]);
 print FixSet(tree, M ! [-3227, -4789, 4661, 7227]);
+
+print Midpoint(tree, Vector(Qp, [1, 1]), Vector(Qp, [1, 0]), Vector(Qp, [1, 12]));
+
+A := IsometryBetweenAxes(tree, Vector(Qp, [1, -1]), Vector(Qp, [1, -1/2]));
+B := IsometryBetweenAxes(tree, Vector(Qp, [1, 2]), Vector(Qp, [1, 0]));
+b1, length := Intersects(A, B);
+b2, path := CrossPath(tree, A, B);
+print b2, path;
+print b1 eq b2 and #path eq length + 1;
